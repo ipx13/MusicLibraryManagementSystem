43fca489dea848966ef4b4795675f0bf949e5baf
XXairship <116432782+ipx13@users.noreply.github.com>
2022-11-19 21:42:49 +0800

    Finalized main menu option 1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def menu():
     print("1 - Get started")
     print("2 - Recommend me something!")
     print("3 - Exit")
+    print("-------------------------")
 
 while True:
     menu()
@@ -53,26 +54,29 @@ while True:
 
                                         elif ch1_conf == 2:
                                             while True:
-                                                print("\nChoose a subfolder to explore:")
+                                                print("\nChoose a subfolder to set as the main folder:")
                                                 for i in os.listdir(mainpath):
                                                     print("> {}".format(i))
-
                                                 print("\nType the folder name")
-                                                explore_fldr = input("Explore: ")
+                                                explore_fldr = input("Set as main folder: ")
                                                 permpath = mainpath + "\{}".format(explore_fldr)
-
-                                                if os.path.exists(permpath):
-                                                    print("\nDisplaying folder contents:")
-                                                    print("> {}".format(permpath))
-                                                    for i in os.listdir(permpath):
-                                                        print(">> {}".format(i))
-
-                                                    print("\nNote: This program can only ask to search deeper ONCE. Please make sure that your artist folders are already accessible.")
-                                                    print("\nSuccessfully saved {} as the main directory for randomization!".format(permpath))
-                                                    break
-                                                else:
-                                                    print("Path not found! Please check your input.")
+                                                try:
+                                                    if os.path.exists(permpath):
+                                                        print("\nDisplaying folder contents:")
+                                                        print("> {}".format(permpath))
+                                                        for i in os.listdir(permpath):
+                                                            print(">> {}".format(i))
+                                                    else:
+                                                        print("Path not found! Please check your input.")
+                                                        continue
+                                                except:
+                                                    print("\nERROR: Make sure you have entered a folder, not a file.")
                                                     continue
+                                                else:
+                                                    print("\nNote: This program can only ask to search deeper ONCE. Please make sure that your artist folders are already accessible.")
+                                                    print("\nSuccessfully saved {} as the main directory for randomization!".format(
+                                                            permpath))
+                                                    break
                                             break
                                     else:
                                         print("Please enter either 1 or 2!")
@@ -82,6 +86,9 @@ while True:
                         else:
                             print("Path not found! Please check your input.")
                             continue
+                    break
+                elif menu_choice == 2:
+                    print("cowabunga")
                 break
             else:
                 print("Please an integer between the range 1-3!")
