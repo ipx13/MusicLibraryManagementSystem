f4ffd4babf348079c26db071690d8a847a58d8fd
XXairship <116432782+ipx13@users.noreply.github.com>
2022-11-19 20:18:55 +0800

    Added a while loop and exception handling for the menu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-print("Welcome to LibRandomizer 1.0! Dear music nerds, are you tired of scrolling through your gi-normous library, looking for something to listen to? Add a folder containing your music library, and we'll fetch a random personalized recommendation for you!")
+print("Welcome to LibRandomizer 1.0! Dear music nerds, are you tired of scrolling through your gi-normous library, looking for something to listen to? Add a folder containing your music library, and we'll fetch a random personalized recommendation for you!\n")
 
 def menu():
     print("Menu: ")
@@ -8,3 +8,12 @@ def menu():
     print("2 - Recommend me something!")
     print("3 - Exit")
 
+while True:
+    menu()
+    while True:
+        try:
+            menu_choice = int(input("What would you like to do? "))
+        except:
+            print("Please enter an integer between 1-3!")
+        else:
+            break
