b93e7de35034e613e5a05ead6f2ffa3c59564918
XXairship <116432782+ipx13@users.noreply.github.com>
2022-11-19 20:14:15 +0800

    Added menu and intro

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import os
 
+print("Welcome to LibRandomizer 1.0! Dear music nerds, are you tired of scrolling through your gi-normous library, looking for something to listen to? Add a folder containing your music library, and we'll fetch a random personalized recommendation for you!")
+
+def menu():
+    print("Menu: ")
+    print("1 - Get started")
+    print("2 - Recommend me something!")
+    print("3 - Exit")
+
